ab1431e8917048bc66abd2121265484ecdf190b9
Mike Labita <mlabita37@gmail.com>
2016-04-23 11:50:08 -0400

    Cleans up js file

--- scripts.js
+++ scripts.js
@@ -48,51 +48,9 @@ $(window).scroll(function() {
     }
 });
 
-// // Type/FadeIn title
-// var names;
-// function setName(name){
-//   names = name.split('');
-// }
-//
-// var currentText = [];
-// var newText;
-// var i=0;
-// setInterval(function(){
-// 	if (i < names.length){
-//   		currentText.push(names[i]);
-//     	newText = currentText.toString();
-//     	newerText = newText.replace(/,/g , "");
-//       $('#hidden').css('display', 'none');
-//       $('#name').css('opacity', '+=0.07');
-//   		$('#name').text(newerText);
-//       $('h2').css('opacity', '+=0.06');
-//       $('address').css('opacity', '+=0.06');
-//   }
-//   i++;
-// }, 200);
-
-// // Heartbeat animation in footer
-// function beatingHeart(){
-// setInterval(function(){
-//   $('#heart').css({
-//    'font-size' : '1.25em',
-//    'margin-top' : '-4px'
-// });
-//   setTimeout(function(){
-//       $('#heart').css({
-//         'font-size' :'1em',
-//         'margin-top' : '0'
-//       });
-//
-//   }, 1000);
-//
-// }, 2000);
-// }
 
 $(function(){
 toggleNav();
 closeAfterClick();
-// beatingHeart();
-// setName("Michael Labita");
 scrollNav();
 });
